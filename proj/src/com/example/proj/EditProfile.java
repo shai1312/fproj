14bc427ecf887490ab7c51ada614116e05f13257
ranigutkin <rani2207@gmail.com>
2014-04-23 18:07:01 +0300

    fjij

--- proj/src/com/example/proj/EditProfile.java
+++ proj/src/com/example/proj/EditProfile.java
@@ -9,21 +9,30 @@ import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.ToggleButton;
 
 public class EditProfile extends Activity {
 	private static final int REQUEST_CODE = 3035;
 	private Boolean buy=true,sale=false;
 	private ExpandableListView catagory;
 	private RadioGroup choose;
+	private ToggleButton car;
+	private Button finish;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_profile);
 		//catagory=(ExpandableListView) findViewById(R.id.catagory);
         choose=(RadioGroup) findViewById(R.id.buy_sale_select);
-       
+        car = (ToggleButton) findViewById(R.id.);
+        finish=(Button) findViewById(R.id.) 
 	}
 	
+	private void ToggleButCar() {
+		
+		
+		ToggleBtCar.on
+		
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
